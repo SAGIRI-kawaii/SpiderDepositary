28caf631190d2a8ab1615efee4eea00fbe25f161
ZAPHAKIEL <45849471+SAGIRI-kawaii@users.noreply.github.com>
2020-10-16 09:59:40 +0800

    Update BangumiTimetable.py

--- bilibili/BangumiTimetable.py
+++ bilibili/BangumiTimetable.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import datetime
 
-async def getNewBangumiJson() -> dict:
+async def get_new_bangumi_json() -> dict:
     url = "https://bangumi.bilibili.com/web_api/timeline_global"
     headers = {
         "accept": "application/json, text/plain, */*",
@@ -21,40 +21,41 @@ async def getNewBangumiJson() -> dict:
     return result
 
 
-async def getNewBangumi() -> list:
-    allBangumiData = await getNewBangumiJson()
-    allBangumiData = allBangumiData["result"][-7:]
-    formatedBangumiData = list()
+async def get_formatted_new_bangumi_json() -> list:
+    all_bangumi_data = await get_new_bangumi_json()
+    all_bangumi_data = all_bangumi_data["result"][-7:]
+    formatted_bangumi_data = list()
 
-    for bangumiData in allBangumiData:
-        tempBangumiDataList = list()
-        for data in bangumiData["seasons"]:
-            tempBangumiDataDict = dict()
-            tempBangumiDataDict["title"] = data["title"]
-            tempBangumiDataDict["cover"] = data["cover"]
-            tempBangumiDataDict["pub_index"] = data["pub_index"]
-            tempBangumiDataDict["pub_time"] = data["pub_time"]
-            tempBangumiDataDict["url"] = data["url"]
-            tempBangumiDataList.append(tempBangumiDataDict)
-        formatedBangumiData.append(tempBangumiDataList)
+    for bangumi_data in all_bangumi_data:
+        temp_bangumi_data_list = list()
+        for data in bangumi_data["seasons"]:
+            temp_bangumi_data_dict = dict()
+            temp_bangumi_data_dict["title"] = data["title"]
+            temp_bangumi_data_dict["cover"] = data["cover"]
+            temp_bangumi_data_dict["pub_index"] = data["pub_index"]
+            temp_bangumi_data_dict["pub_time"] = data["pub_time"]
+            temp_bangumi_data_dict["url"] = data["url"]
+            temp_bangumi_data_list.append(temp_bangumi_data_dict)
+        formatted_bangumi_data.append(temp_bangumi_data_list)
 
-    return formatedBangumiData
+    return formatted_bangumi_data
 
 
-async def formattedOutputBangumi(days: int) -> str:
-    formatedBangumiData = await getNewBangumi()
-    outputStr = "------BANGUMI------\n"
+async def formatted_output_bangumi(days: int) -> str:
+    formatted_bangumi_data = await get_formatted_new_bangumi_json()
+    temp_output_substring = ["------BANGUMI------\n"]
     now = datetime.datetime.now()
     for index in range(days):
-        outputStr += now.strftime("\n%m-%d 即将播出：")
-        for data in formatedBangumiData[index]:
-            outputStr += "\n%s %s %s\nurl:%s\n" % (data["pub_time"], data["title"], data["pub_index"], data["url"])
-        outputStr += "\n\n----------------\n"
+        temp_output_substring.append(now.strftime("\n%m-%d 即将播出："))
+        for data in formatted_bangumi_data[index]:
+            temp_output_substring.append("\n%s %s %s\n" % (data["pub_time"], data["title"], data["pub_index"]))
+            temp_output_substring.append("url:%s\n" % (data["url"]))
+        temp_output_substring.append("\n\n----------------\n")
         now += datetime.timedelta(days=1)
-    return outputStr
+    return "".join(temp_output_substring)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(formattedOutputBangumi(2))
+    result = loop.run_until_complete(formatted_output_bangumi(2))
     print(result)
